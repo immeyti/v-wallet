98d4892575afe154360238beaf06d4e6b9cab8a0
immeyti <mehdi.samirakh68@gmail.com>
2020-12-26 00:49:20 +0330

    Added a trait and a few helper method

---
+++ src/Traits/HasWallet.php
@@ -0,0 +1,56 @@
+<?php
+
+
+namespace Immeyti\VWallet\Traits;
+
+
+use Immeyti\VWallet\Wallet;
+
+trait HasWallet
+{
+
+    public function getWallets()
+    {
+        return Wallet::getWallets($this->getkey());
+    }
+
+    public function balance($coin)
+    {
+        $walletModel = Wallet::getWallets([
+            'user_id' => $this->getkey(),
+            'coin' => $coin
+        ]);
+
+        if (! $walletModel->count())
+            return 0;
+
+        return $walletModel->first()->balance;
+    }
+
+    public function deposit($coin, $amount, $meta = [])
+    {
+        $walletModel = Wallet::getWallets([
+            'user_id' => $this->getkey(),
+            'coin' => $coin
+        ])->first();
+
+        if (! $walletModel)
+            $walletModel = Wallet::create($this->getkey(), $coin);
+
+
+        return Wallet::deposit($walletModel, $amount, $meta);
+
+    }
+
+    public function withdraw($coin, $amount, $meta = [])
+    {
+        $walletModel = Wallet::getWallets([
+            'user_id' => $this->getkey(),
+            'coin' => $coin
+        ])->first();
+
+
+        return Wallet::withdraw($walletModel, $amount, $meta);
+
+    }
+}
